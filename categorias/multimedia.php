09c4d6eac18837d54ffb82081a879748b4ffe6f8
jiambn13 <jiam.blanco@gmail.com>
2022-11-06 18:45:35 -0500

    nuevas ideas

--- categorias/multimedia.php
+++ categorias/multimedia.php
@@ -4,10 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sección Multimedia</title>
+    <title>Atencion Psicologica</title>
 
     <!-- Bootstrap core CSS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@400;600;700&family=Source+Serif+Pro:ital,wght@0,600;0,700;1,300&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/estudiantil/intranet/css/grupos.css">
+    
+    
 
     <style>
     body { padding-top: 50px; }
@@ -20,33 +26,58 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbars" aria-expanded="false" aria-controls="navbars">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Trading Academy</a>
+          <a class="navbar-brand" href="#" >Atencion Psicologica</a>
         </div>
-        <div id="navbar" class="collapse navbar-collapse">
+        <div id="navbars" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
           </ul>
-        </div><!--/.nav-collapse -->
+        </div>
       </div>
     </nav>
+    
 
     <div class="container">
 
       <div class="starter-template">
-        <h1>Sección Multimedia</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+        <h1>Atencion Psicologica</h1>
+        <p class="lead">Ante una situación como la que nos está tocando vivir, hay razones para que puedas sentirte vulnerable. Todas y todos nos estamos 
+          sintiendo así, de un modo u otro. Manejar la incertidumbre, el caos, la confusión o los miedos, tan comunes en una situación como ésta, no es una 
+          tarea sencilla. Es como si nuestro estilo de vida, nuestro espacio de seguridad, corriera el riesgo de dejar de serlo. No podemos evitar lo que ocurre, 
+          pero sí tenemos alguna posibilidad de elegir la respuesta que damos ante lo que nos ocurre.<br> </p>
       </div>
 
-    </div><!-- /.container -->
+      <section  class="section">
+      <div class="container-section">
+        <h2 class="title">
+              Cuentanos    
+        </h2>
+        <div class="seccion__contenido">
+              <form id="form" class="form1">
+                    <div class="form-group1">
+                          <label for="name">Nombre</label>
+                          <input type="text" id="name" class="form-control1" placeholder="Ingresa tu nombre" required>
+                    </div>
+                    <div class="form-group1">
+                          <label for="email">Correo</label>
+                          <input type="email" id="email" class="form-control1" placeholder="Ingresa tu correo" required>
+                    </div>
+                    <div class="form-group1">
+                          <label for="message">Mensaje</label>
+                          <textarea id="message" class="form-control1" placeholder="Platicame tu idea genial" rows="10"></textarea>
+                    </div>
+                    <input type="submit" class="btn-form" value="Enviar" />
+              </form>
+        </div>
+  </div>
+    </section>
 
+    </div>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
