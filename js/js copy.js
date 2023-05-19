35ee0073904551d509f445f7c0aa3b2027bbb5b9
David Garzon <picassogarzon@gmail.com>
2023-05-19 09:03:25 -0500

    trabajo

---
+++ js/js copy.js
@@ -0,0 +1,21 @@
+let slideIndex = 0;
+showSlide(slideIndex);
+
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  const slides = document.getElementsByClassName("slide");
+  if (n >= slides.length) {
+    slideIndex = 0;
+  } else if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.transform = `translateX(-${slideIndex *100}%)`;
+  }
+}
+setInterval(() =>{changeSlide(1)},5000)
+
